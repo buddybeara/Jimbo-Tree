70fd9198ef6ee74741a743c84be7dab992e4a19c
gaimn <gaimnbro9@gmail.com>
2025-04-19 18:10:59 +0100

    round 2
    my brain has been severely damaged

--- js/layers.js
+++ js/layers.js
@@ -16,6 +16,7 @@ addLayer("j", {
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
 		if (hasUpgrade("j",13)) mult = mult.times(upgradeEffect("j",13))
+		if (hasUpgrade("f",12)) mult = mult.times(upgradeEffect("f",12))
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
@@ -25,15 +26,15 @@ addLayer("j", {
     hotkeys: [
         {key: "j", description: "J: Reset for jimbos", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
-	upgrades: { // CHANGE UPGRADE TEXT I FORGOR :sob:
+	upgrades: { // FIXED UPGRADE TEXT
 		11: {
 			title: "New Beginnings",
-			description: "Jimbos generate.",
+			description: "Hotsauce generates.",
 			cost: new Decimal(1),
 		},
 		12: {
 			title: "Prestigious Hotsauce",
-			description: "Jimbo gain is multiplied by Prestiged Jimbos.",
+			description: "Hotsauce gain is multiplied by Jimbos.",
 			cost: new Decimal(1),
 			effect() {
 				effect = player.j.points.add(1).pow(0.4)
@@ -44,7 +45,7 @@ addLayer("j", {
 		},
 		13: {
 			title: "Interesting Synergy",
-			description: "Prestiged Jimbo gain is multiplied by Jimbo gain.",
+			description: "Jimbo gain is multiplied by hotsauce gain.",
 			cost: new Decimal(3),
 			effect() {
 				effect = getPointGen().log(2).add(1)
@@ -55,7 +56,7 @@ addLayer("j", {
 		},
 		21: {
 			title: "Antiwalled",
-			description: "Jimbo gain boosts itself.",
+			description: "Hotsauce gain boosts itself.",
 			cost: new Decimal(5),
 			effect() {
 				effect = player.points.add(1).log(10).add(1)
@@ -67,7 +68,7 @@ addLayer("j", {
 		},
 		22: {
 			title: "Empowering",
-			description: "Prestiged Jimbos boost the upgrade to the left and right.",
+			description: "Jimbos boost the upgrade to the left and right.",
 			cost: new Decimal(8),
 			effect() {
 				effect = player.j.points.add(1).log(100).add(1).pow(0.5)
@@ -78,7 +79,7 @@ addLayer("j", {
 		},
 		23: {
 			title: "Reversed Hotsauce",
-			description: "Prestiged Jimbo gain is boosted by Jimbos.",
+			description: "Jimbo gain is boosted by hotsauce.",
 			cost: new Decimal(5),
 			effect() {
 				effect = player.points.add(1).log(15).add(1).pow(0.5).add(1)
@@ -87,6 +88,58 @@ addLayer("j", {
 			},
 		  effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" },
 		  unlocked() { return hasUpgrade("j",12)&&hasUpgrade("j",13) }
+		},
+	},
+    layerShown(){return true}
+})
+addLayer("f", {
+    name: "factory", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "F", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+    color: "#a3a2a5",
+    requires: new Decimal(100), // Can be a function that takes requirement increases into account
+    resource: "factories", // Name of prestige currency
+    baseResource: "jimbos", // Name of resource prestige is based on
+    baseAmount() {return player.j.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 1/3, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "f", description: "F: Reset for factories", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+	upgrades: { // CHANGE UPGRADE TEXT I FORGOR :sob:
+		11: {
+			title: "Production Line",
+			description: "Factories boost hotsauce.",
+			cost: new Decimal(1),
+			effect() {
+				effect = player.f.points.add(1).log(5).add(1)
+				return effect
+			},
+		  effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" },
+		  unlocked() { return true }
+		},
+		12: {
+			title: "Engineering",
+			description: "Factories now boost Jimbos very slightly.",
+			cost: new Decimal(1),
+			effect() {
+				effect = player.f.points.add(1).log(100).add(1)
+				return effect
+			},
+		  effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" },
+		  unlocked() { return true }
 		},
 	},
     layerShown(){return true}
--- js/mod.js
+++ js/mod.js
@@ -44,6 +44,7 @@ function getPointGen() {
 	let gain = new Decimal(1)
 	if (hasUpgrade("j",12)) { gain = gain.times(upgradeEffect("j",12)) } 
 	if (hasUpgrade("j",21)) { gain = gain.times(upgradeEffect("j",21)) } 
+	if (hasUpgrade("f",11)) { gain = gain.times(upgradeEffect("f",11)) }
 	return gain
 }
 
