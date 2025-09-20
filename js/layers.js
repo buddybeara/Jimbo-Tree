b0b30f8a0cbf62375af3926e882d214c57ef92d9
gaimn <104718139+gaimn@users.noreply.github.com>
2025-09-20 18:20:16 +0100

    Update layers.js
    fix languag

--- js/layers.js
+++ js/layers.js
@@ -9,7 +9,7 @@ addLayer("j", {
     color: "#f0a3d0",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "jimbos", // Name of prestige currency
-    baseResource: "points", // Name of resource prestige is based on
+    baseResource: "hotsauces", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 1/3, // Prestige currency exponent
